24485c4fdc90910b890bfa2e407928b76187d386
Kirill-2-3 <87173384+Kirill-2-3@users.noreply.github.com>
2021-08-21 11:48:45 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+$(function() {
+
+
+    /* Slick slider */
+
+    $('#home__slick').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 500,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        cssEase: 'linear',
+      });
+
+      $('#materials__slick').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        cssEase: 'linear',
+      });
+
+
+      /* Slick scroll */
+
+      $("#arrow-down").on("click", function(e) {
+        e.preventDefault();
+
+        let elementHeight = $(".page").height();
+
+        $("html, body").animate({
+            scrollTop: elementHeight - 60
+        }, 700);
+
+        $(nav).removeClass("show");
+        $(header).removeClass("show");
+      });
+
+
+});
